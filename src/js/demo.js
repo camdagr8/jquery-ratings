dedd63d83c7e94763d5cba5fc6e49c9042160baa
Cameron Tullos <ctullos@resourceammirati.com>
2016-11-10 08:20:20 -0500

    Fixed bug in demo script after changing variable names.

--- src/js/demo.js
+++ src/js/demo.js
@@ -2,34 +2,34 @@
 $(function() {
 
 	$('#state-toggler').on('click', function () {
-		var elm = $('#stars-2');
-		var enabled = elm.stars('get', 'enabled');
+		var elm = $('#ratings-2');
+		var enabled = elm.ratings('get', 'enabled');
 		var state = (enabled === true) ? 'disable' : 'enable';
 		var label = (enabled === true) ? 'enable' : 'disable';
 
-		elm.stars(state);
+		elm.ratings(state);
 		$(this).text(label);
 	});
 
 	$('#max-toggler').on('click', function () {
-		var elm = $('#stars-1');
-		var max = elm.stars('get', 'max');
+		var elm = $('#ratings-1');
+		var max = elm.ratings('get', 'max');
 			max = (max === 5) ? 10 : 5;
 
 		var lbl = (max === 10) ? 5 : 10;
 
-		elm.stars('max', max);
+		elm.ratings('max', max);
 		$(this).text('max ' + lbl);
 	});
 
 	$('#val-toggler').on('click', function () {
-		var elm = $('#stars-1');
-		var val = elm.stars('get', 'value');
+		var elm = $('#ratings-1');
+		var val = elm.ratings('get', 'value');
 			val = (val === 5) ? 0 : 5;
 
 		var lbl = (val === 0) ? 5 : 0;
 
-		elm.stars('value', val);
+		elm.ratings('value', val);
 		$(this).text('value ' + lbl);
 	});
 
